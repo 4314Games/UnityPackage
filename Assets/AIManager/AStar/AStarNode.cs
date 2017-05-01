73a721e22a03182594ad7c418edb6c19d1cbda39
KyleN97 <kyle.norton1@live.com>
2017-05-01 15:34:05 +1000

    A Star
    Stated A Star dev + rollback to Unity 5.5.2f1

---
+++ Assets/AIManager/AStar/AStarNode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AStarNode
+{
+    public bool walkable;
+    public Vector3 worldPosition;
+
+    public AStarNode(bool _walkable,Vector3 _worldPos)
+    {
+        walkable = _walkable;
+        worldPosition = _worldPos;
+    }
+}
---
+++ Assets/AIManager/AStar/Grid.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+    public Transform objectToAffect;
+    public LayerMask unwalkableMask;
+    public Vector2 gridWorldSize;
+    public float nodeRadius;
+    AStarNode[,] grid;
+
+	float nodeDiameter;
+	int gridSizeX, gridSizeY;
+
+    void Start()
+    {
+		nodeDiameter = nodeRadius * 2;
+		gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+		gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+
+		CreateGrid ();
+
+    }
+	void CreateGrid()
+	{
+		grid = new AStarNode[gridSizeX, gridSizeY];
+		Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x/2 - Vector3.forward * gridWorldSize.y/2;
+
+		for (int x = 0; x < gridSizeX; x++) {
+			for (int y = 0; y < gridSizeY; y++) {
+				Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+                bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius,unwalkableMask));
+                grid[x, y] = new AStarNode(walkable, worldPoint);
+            }
+		}
+	}
+    public AStarNode NodeFromWorldPoint(Vector3 worldPosition)
+    {
+        float percentX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
+        float percentY = (worldPosition.z + gridWorldSize.y / 2) / gridWorldSize.y;
+
+        percentX = Mathf.Clamp01(percentX);
+        percentX = Mathf.Clamp01(percentY);
+        
+        int x = Mathf.RoundToInt((gridSizeX-1) * percentX);
+        int y = Mathf.RoundToInt((gridSizeY-1) * percentY);
+
+        return grid[x, y];
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
+        if (grid != null)
+        {
+            AStarNode objectToAffectNode = NodeFromWorldPoint(objectToAffect.position);
+            foreach (AStarNode item in grid)
+            {
+                Gizmos.color = (item.walkable) ? Color.white : Color.red;
+                if (objectToAffectNode == item)
+                {
+                    Gizmos.color = Color.cyan;
+                }
+                Gizmos.DrawCube(item.worldPosition, Vector3.one * (nodeDiameter - 0.1f));
+            }
+        }
+    }
+}
